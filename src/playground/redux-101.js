19997be03c72f53352336eee9a841999ef2def67
andela-fomokaro <faith.omokaro@andela.com>
2018-10-11 00:45:53 +0100

    create action generators

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,17 +1,40 @@
 import { createStore } from 'redux';
 
+// Action generators --- Function that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count } = {}) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET',
+});
+
 const store = createStore( (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return  {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return  {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
+    case 'SET': 
+      return {
+        count: action.count
+      }
     case 'RESET':
       return  {
         count: 0
@@ -30,20 +53,14 @@ const unsubscribe = store.subscribe(() => {
 
 // unsubscribe(); // Used if you want to unsubscribe
 
+// I'd like to set count
+store.dispatch(setCount({ count: 101 }));
 
 // I'd like to increment the count
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 })) ;
 
 // I'd like to decrement the count
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10 })) ;
 
 // I'd like to reset the count
-store.dispatch({
-  type: 'RESET',
-});
+store.dispatch(resetCount()) ;
