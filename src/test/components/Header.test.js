17718c090a6fb114c1d27fa8678a6d7658fe4fa9
andela-fomokaro <faith.omokaro@andela.com>
2018-10-15 17:39:44 +0100

    handle testing of header component using ReactShallowRender

---
+++ src/test/components/Header.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactShallowRender from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
+
+test('should render Header Successfully', () => {
+  const renderer = new ReactShallowRender();
+  renderer.render(<Header/>)
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
